b0c07c8be309c807e79a1ad2caa0b0d5f8143813
xetosu <96944796+xetosu@users.noreply.github.com>
2022-01-03 20:51:31 +0500

    Add files via upload
    add some fucking files

---
+++ modules/social-links.js
@@ -0,0 +1,12 @@
+import { icons } from './icons'
+
+export const socialLinks = [
+  {
+    icon: icons.github,
+    url: 'https://github.com/xenmitsu'
+  },
+  {
+    icon: icons.telegram,
+    url: 'https://t.me/xenmitsu'
+  }
+]
